15c8c4073c2a95ad8590163b96f7fd1ef1b8237f
Tamires Manhães <tamiresmanhaes4@gmail.com>
2020-08-23 17:12:16 -0300

    adicionando novas rotas das novas páginas

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Logon from './pages/Logon';
-import Home from './pages/Home';
 import Register from './pages/Register';
+import Home from './pages/Home';
+import Alunos from './pages/Alunos';
+import NovoAluno from './pages/NovoAluno';
+import Professores from './pages/Professores';
+import NovoProfessor from './pages/NovoProfessor';
+import Classes from './pages/Classes';
 import Page404 from './pages/404';
 
 
@@ -11,9 +16,14 @@ export default function Routes(){
   return(
     <BrowserRouter>
       <Switch>
-      <Route path="/" exact component={Logon}/>
-        <Route path="/home" exact component={Home}/>
+        <Route path="/" exact component={Logon}/>
         <Route path="/register" exact component={Register}/>
+        <Route path="/home" exact component={Home}/>
+        <Route path="/alunos" exact component={Alunos}/>
+        <Route path="/aluno/novo" exact component={NovoAluno}/>
+        <Route path="/professores" exact component={Professores}/>
+        <Route path="/professor/novo" exact component={NovoProfessor}/>
+        <Route path="/classes" exact component={Classes}/>
         <Route exact component={Page404}/>
       </Switch>
     </BrowserRouter>
