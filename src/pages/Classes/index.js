0827e45762a8f191577883a77670a9b751197768
Tamires Manhães <tamiresmanhaes4@gmail.com>
2020-08-23 17:07:12 -0300

    adicionando página de Classes

---
+++ src/pages/Classes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss';
+
+export default function Classes(){
+  return (
+    <>
+      <h1>Classes</h1>
+      <Link to="/">voltar para home</Link>
+    </>
+  )
+}
