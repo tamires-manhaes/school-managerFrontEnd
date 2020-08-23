a415fb14f641fb080cf60f960b8ae7915a1039f9
Tamires Manhães <tamiresmanhaes4@gmail.com>
2020-08-23 17:09:41 -0300

    refatorando a Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import './styles.scss';
+import { Link } from 'react-router-dom';
+import { AiFillHome, AiOutlinePoweroff } from 'react-icons/ai'
 
-import MenuHome from './components/MenuHome';
 import Header from '../../components/Header';
+import Button from '../../components/SmallButton';
+import MenuRelatorio from '../../components/MenuRelatorio';
 import Footer from '../../components/Footer';
 
 export default function Home(){
   return(
     <div className="homeContainer">
       <div className="homeContent">
-      <Header />
-      <MenuHome />  
+        <Header>
+          <Button>
+            <Link to="/home">
+              <AiFillHome size={20} color="#FFFFFF"/>
+            </Link>
+          </Button>
+
+          <Button>
+            <Link to="/">
+              <AiOutlinePoweroff size={20} color="#FFFFFF"/>
+            </Link>
+          </Button>
+        </Header>
+        
+        <MenuRelatorio>
+          <li>
+            <Link to="/alunos">Alunos</Link>
+          </li>
+
+          <li>
+            <Link to="/professores">Professores</Link>
+          </li>
+
+          <li>
+            <Link to="/classes">Classes</Link>
+          </li>
+        </MenuRelatorio>
       </div>
       <Footer />
-    </div>
+  </div>
   )
 }
